7ec7debf01d704b7bf6c12452358ebaff43c0e33
Team taxidriver <taxidriver@stackeffect.de>
2020-07-05 15:35:21 +0200

    Send a sample HTTP request
    The code does roughly the same thing as starterkit-python.
    https://github.com/icfpcontest2020/starterkit-python/blob/master/app/main.py

--- app/main.rb
+++ app/main.rb
@@ -1,1 +1,15 @@
-puts ARGV[0] + " " + ARGV[1]
+require 'net/http'
+
+def main
+  serverurl = ARGV[0]
+  playerkey = ARGV[1]
+  puts "ServerUrl: %s; PlayerKey: %s" % [serverurl, playerkey]
+
+  uri = URI(serverurl + "?playerKey=#{playerkey}")
+  res = Net::HTTP.get_response(uri)
+  puts res
+end
+
+if __FILE__ == $0
+  main
+end
